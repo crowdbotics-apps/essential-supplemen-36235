d075a866ab4ac087e349cf6766dfadc0d664c154
Crowdbotics <team@crowdbotics.com>
2022-07-17 11:41:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/essential_supplemen_36235/MainActivity.java
@@ -0,0 +1,15 @@
+package com.essential_supplemen_36235;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "essential_supplemen_36235";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "essential_supplemen_36235",
+  url: "https://essential_supplemen_36235.botics.co",
+  api: "https://essential_supplemen_36235.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
